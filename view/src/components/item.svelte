ca9f490c891cc10735e9b2a12306b08f9902fec4
scupdev <ot.gaming132@gmail.com>
2023-01-30 10:57:23 +0530

    working on the frontend now..

---
+++ view/src/components/item.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Card from "./card.svelte";
+</script>
+
+<Card>
+  <p>Smol: https://localhost:3000/r/</p>
+  <p>Redirect: {"redirect"}</p>
+  <p>Clicked:</p>
+  <button class="update">Update</button>
+  <button class="delete">Delete</button>
+</Card>
+
+<style>
+    button {
+        color: white;
+        font-weight: bold;
+        border: none;
+        padding: .75rem;
+        border-radius: 4px;
+    }
+
+    .update {
+        background-color: yellowgreen;
+    }
+
+    .delete {
+        background-color: red;
+    }
+</style>
